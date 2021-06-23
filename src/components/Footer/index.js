5aeb5fee24643df2fee17647516ac3032198186f
rafsan99 <rafsan.redwan69@gmail.com>
2021-06-23 18:56:13 +0600

    First Commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import { FaFacebook, FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { 
+    FooterContainer,
+    FooterWrap,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinkItems,
+    FooterLinkTitle,
+    FooterLink,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo,
+    WebsiteRights,
+    SocialIcons,
+    SocailIconLink
+} from './FooterElements';
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About US</FooterLinkTitle>
+                                <FooterLink to='/'>How it works</FooterLink>
+                                <FooterLink to='/'>Testimonials</FooterLink>
+                                <FooterLink to='/'>Careers</FooterLink>
+                                <FooterLink to='/'>Investors</FooterLink>
+                                <FooterLink to='/'>Terms of service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Contact US</FooterLinkTitle>
+                                <FooterLink to='/'>Contact</FooterLink>
+                                <FooterLink to='/'>Support</FooterLink>
+                                <FooterLink to='/'>Destinations</FooterLink>
+                                <FooterLink to='/'>Sponsorships</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                        <FooterLinkTitle>Videos</FooterLinkTitle>
+                                <FooterLink to='/'>Submit video</FooterLink>
+                                <FooterLink to='/'>Ambassadors</FooterLink>
+                                <FooterLink to='/'>Agency</FooterLink>
+                                <FooterLink to='/'>Influencer</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Social Media</FooterLinkTitle>
+                                <FooterLink to='/'>Instagram</FooterLink>
+                                <FooterLink to='/'>Facebook</FooterLink>
+                                <FooterLink to='/'>YouTube</FooterLink>
+                                <FooterLink to='/'>Twitter</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/'>
+                            dolla
+                        </SocialLogo>
+                        <WebsiteRights>dolla Ⓒ {new Date().getFullYear()}
+                        All rights reserved.</WebsiteRights>
+                        <SocialIcons>
+                            <SocailIconLink href='/' target='_blank' 
+                            aria-label='Facebook'>
+                                <FaFacebook />
+                            </SocailIconLink>
+                            <SocailIconLink href='/' target='_blank' 
+                            aria-label='Instagram'>
+                                <FaInstagram />
+                            </SocailIconLink>
+                            <SocailIconLink href='/' target='_blank' 
+                            aria-label='Youtube'>
+                                <FaYoutube />
+                            </SocailIconLink>
+                            <SocailIconLink href='/' target='_blank' 
+                            aria-label='Twitter'>
+                                <FaTwitter />
+                            </SocailIconLink>
+                            <SocailIconLink href='/' target='_blank' 
+                            aria-label='Linkedin'>
+                                <FaLinkedin />
+                            </SocailIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
+            </FooterWrap>
+        </FooterContainer>
+    )
+}
+
+export default Footer;
---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,59 @@
+
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fee.',
+    description: 
+        'Get access to our exclusive app that aloows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg').default,
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
+
+
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headline: 'Login to your account at any time',
+    description: 
+        'We have you covered no matter where you are located. All you need is an internet connection and a phone or computer.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/svg-2.svg').default,
+    alt: 'Piggybank',
+    dark: false,
+    primary: false,
+    darkText: true
+};
+
+
+
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join Our Team',
+    headline: 'Creating an account is extremely easy',
+    description: 
+        'Get everything set up and ready in under 10 minutes.',
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: require('../../images/svg-4.svg').default,
+    alt: 'Paper',
+    dark: false,
+    primary: false,
+    darkText: true
+};
